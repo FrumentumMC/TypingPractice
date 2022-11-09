5901353afd2e2e2d7e2dc21eb2926fba0204028f
FrumentumMC <111873774+FrumentumMC@users.noreply.github.com>
2022-11-09 21:42:16 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -19,7 +19,6 @@ function startTimer() {
 }
 document.getElementById("timer").innerHTML="1:00";
 
-
 const text = document.getElementById("text-generator");
 
 function getText() {
@@ -28,7 +27,6 @@ function getText() {
     .then(data => {text.innerHTML = `${data.content}`})
 }
 getText();
-
 
 function resetGame() {
     document.getElementById("text-input").value = "";
@@ -39,3 +37,86 @@ function resetGame() {
 function getResults() {
     document.getElementById("result-details").style.display = "block";
 }
+
+let keys = document.querySelectorAll(".key");
+let shift_left = document.querySelector("#shift-left");
+let shift_right = document.querySelector("#shift-right");
+let ctrl_left = document.querySelector("#ctrl-left");
+let alt_left = document.querySelector("#alt-left");
+let space = document.querySelector("#space");
+let alt_right = document.querySelector("#alt-right");
+let ctrl_right = document.querySelector("#ctrl-right");
+
+for (let i = 0; i < keys.length; i++) {
+    keys[i].setAttribute("keyname", keys[i].innerText);
+    keys[i].setAttribute("lowerCaseName", keys[i].innerText.toLowerCase());
+}
+
+window.addEventListener("keydown", function(e) {
+    for (let i = 0; i < keys.length; i++) {
+        if (e.key == keys[i].getAttribute("keyname") || e.key == keys[i].getAttribute("lowerCaseName")) {
+            keys[i].classList.add("active-keys")
+        }
+        if (e.code == "ShiftLeft") {
+            shift_right.classList.remove("active-keys")
+        }
+        if (e.code == "ShiftRight") {
+            shift_left.classList.remove("active-keys")
+        }
+        if (e.code == "CtrlLeft") {
+            ctrl_right.classList.remove("active-keys")
+        }
+        if (e.code == "AltLeft") {
+            alt_right.classList.remove("active-keys")
+        }
+        if (e.code == "Space") {
+            space.classList.add("active-keys")
+        }
+        if (e.code == "AltRight") {
+            alt_left.classList.remove("active-keys")
+        }
+        if (e.code == "CtrlRight") {
+            ctrl_left.classList.remove("active-keys")
+        }
+    }
+})
+
+window.addEventListener("keyup", function(e) {
+    for (let i = 0; i < keys.length; i++) {
+        if (e.key == keys[i].getAttribute("keyname") || e.key == keys[i].getAttribute("lowerCaseName")) {
+            keys[i].classList.remove("active-keys")
+            keys[i].classList.add("remove")
+        }
+        if (e.code == "ShiftLeft") {
+            shift_right.classList.remove("active-keys")
+            shift_right.classList.remove("remove")
+        }
+        if (e.code == "ShiftRight") {
+            shift_left.classList.remove("active-keys")
+            shift_left.classList.remove("remove")
+        }
+        if (e.code == "CtrlLeft") {
+            ctrl_right.classList.remove("active-keys")
+            ctrl_right.classList.remove("remove")
+        }
+        if (e.code == "AltLeft") {
+            alt_right.classList.remove("active-keys")
+            alt_right.classList.remove("remove")
+        }
+        if (e.code == "Space") {
+            space.classList.remove("active-keys")
+            space.classList.add("remove")
+        }
+        if (e.code == "AltRight") {
+            alt_left.classList.remove("active-keys")
+            alt_left.classList.remove("remove")
+        }
+        if (e.code == "CtrlRight") {
+            ctrl_left.classList.remove("active-keys")
+            ctrl_left.classList.remove("remove")
+        }
+        setTimeout(()=> {
+            keys[i].classList.remove("remove")
+        }, 200)
+    }
+})
