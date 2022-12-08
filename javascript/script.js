5b4ebc88a31b3a63f93fa22bdfdca789b2b32b06
FrumentumMC <111873774+FrumentumMC@users.noreply.github.com>
2022-12-08 14:43:53 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -12,3 +12,24 @@ const navMenu = document.querySelector('.nav-responsive');
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('nav-active');
 })
+
+function languageDropdown() {
+    document.getElementById('dropdown-id').classList.toggle('show-dropdown');
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName('dropdown-content');
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show-dropdown')) {
+                openDropdown.classList.remove('show-dropdown');
+            }
+        }
+    }
+}
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
